a4a67bb253b75d94fe23d2b14b82f773a0b73b2a
Aman11b <amansingh1100b@gmail.com>
2026-05-28 13:06:19 +0530

    working with action creator

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 //  pure redux code
-import { type } from "@testing-library/user-event/dist/type";
 import { createStore } from "redux";
 
 const initialState = {
@@ -7,9 +6,11 @@ const initialState = {
   loan: 0,
   loanPurpose: "",
 };
+
+const ACCOUNT_DEPOSIT = "account/deposite";
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case "account/deposite":
+    case ACCOUNT_DEPOSIT:
       return { ...state, balance: state.balance + action.payload };
 
     case "account/withdraw":
@@ -39,24 +40,58 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposite", payload: 500 });
+// store.dispatch({ type: "account/deposite", payload: 500 });
 
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: {
+//     amount: 1000,
+//     loanPurpose: "Buy a car",
+//   },
+// });
+
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+
+// console.log(store.getState());
+
+function deposite(amount) {
+  return { type: ACCOUNT_DEPOSIT, payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: {
+      amount: amount,
+      loanPurpose: purpose,
+    },
+  };
+}
+
+function payloan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposite(500));
 console.log(store.getState());
 
-store.dispatch({ type: "account/withdraw", payload: 200 });
-
+store.dispatch(withdraw(100));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: {
-    amount: 1000,
-    loanPurpose: "Buy a car",
-  },
-});
-
+store.dispatch(requestLoan(1000, "buy a car"));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
-
+store.dispatch(payloan());
 console.log(store.getState());
